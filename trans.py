f9518112824fab35b277aaf8700f559983afd7b8
isdnhinet <107520881+isdnhinet@users.noreply.github.com>
2026-06-23 09:29:11 +0800

    Create trans.py

---
+++ trans.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import json
+
+FIELD_MAP = {
+    "證券代號": "Code",
+    "證券名稱": "Name",
+    "成交股數": "TradeVolume",
+    "成交金額": "TradeValue",
+    "開盤價": "OpeningPrice",
+    "最高價": "HighestPrice",
+    "最低價": "LowestPrice",
+    "收盤價": "ClosingPrice",
+    "漲跌價差": "Change",
+    "成交筆數": "Transaction",
+}
+
+def convert_old_to_new(old):
+    date = old["date"]
+    new_data = []
+    for row in old["data"]:
+        item = { "Date": date }
+
+        for field, value in zip(old["fields"], row):
+            if field in FIELD_MAP:
+                item[FIELD_MAP[field]] = value
+        
+        new_data.append(item)
+    return new_data
+
+output_dir = Path("dayall")
+output_dir.mkdir(exist_ok=True)
+
+for file in Path("data").glob("*.json"):
+    with open(file, encoding="utf-8") as f:
+        data = json.load(f)
+
+    if isinstance(data, list):
+        print("Already new format")
+        continue
+
+    new_data = convert_old_to_new(data)
+
+    output_path = output_dir / file.name
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(new_data, f, ensure_ascii=False, indent=2)
+
+    print("converted -> {output_path}")
